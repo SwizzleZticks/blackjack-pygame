030aa8747d2cc9fe3dd795e7bd02a1b36d2caeed
Josh <jddichello@gmail.com>
2025-03-28 18:46:58 -0500

    create deck class and functionality to populate deck

---
+++ deck.py
@@ -0,0 +1,18 @@
+import card
+
+
+def _create_deck():
+    deck = list()
+
+    for suit in card.Suit:
+        for rank in card.Rank:
+            deck.append(card.Card(rank, suit))
+
+    return deck
+
+
+class Deck:
+    def __init__(self):
+        self.deck = _create_deck()
+
+
