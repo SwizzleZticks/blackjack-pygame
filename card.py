3dc2ff0e382ae4cd927e3c12b2fdd30097fcbbe0
Josh <jddichello@gmail.com>
2025-03-29 11:23:44 -0500

    cleanup card class, adjust ace value

--- card.py
+++ card.py
@@ -1,26 +1,25 @@
 from enum import Enum
 
 class Rank(Enum):
-    ACE = 1
-    TWO = 2
+    TWO   = 2
     THREE = 3
-    FOUR = 4
-    FIVE = 5
-    SIX = 6
+    FOUR  = 4
+    FIVE  = 5
+    SIX   = 6
     SEVEN = 7
     EIGHT = 8
-    NINE = 9
-    TEN = 10
-    JACK = 10
+    NINE  = 9
+    TEN   = 10
+    JACK  = 10
     QUEEN = 10
-    KING = 11
-
+    KING  = 10
+    ACE   = 11
 
 class Suit(Enum):
-    HEARTS = 0
+    HEARTS   = 0
     DIAMONDS = 1
-    SPADES = 2
-    CLUBS = 3
+    SPADES   = 2
+    CLUBS    = 3
 
 class Card:
     def __init__(self, rank: Rank, suit: Suit):
