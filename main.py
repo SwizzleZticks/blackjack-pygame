b607dd359694cac947c4d7901d2a0222c498be42
Josh <jddichello@gmail.com>
2025-03-30 10:47:24 -0500

    testing player, dealer, and deck functions

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from dealer import Dealer
 from deck import Deck
+from player import Player
+
 
 def main():
     deck = Deck()
-    print(list(deck.deck))
+    dealer = Dealer(deck)
+    player = Player()
+    player.hit(dealer.draw_card())
+
+    print(player.get_score())
 
 main()
